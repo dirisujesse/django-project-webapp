1f9257f3e9ad7cd4ba4d5d32efefc119f08121c2
Dirisu Jesse <dirisujesse@gmail.com>
2017-03-30 13:34:59 +0100

    Update settings.py

--- webapp/settings.py
+++ webapp/settings.py
@@ -25,7 +25,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'ffi10k!mot7@_ut*oqz(c!nucy03sh13i+g1wkyb7^x!x04mb3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 SITE_ID = 1
 
