803e209da90b138a3a3984bc678b4563fa2fc601
Dirisu Jesse <dirisujesse@gmail.com>
2017-03-30 13:41:03 +0100

    Update sitemaps.py

--- polls/sitemaps.py
+++ polls/sitemaps.py
@@ -4,7 +4,7 @@ from .models import Question
 
 class PollSiteMap(Sitemap):
     changefreq = 'weekly'
-    priority = 0.9
+    priority = 0.8
 
     def items(self):
         return Question.objects.all()
