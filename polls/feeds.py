84679b51eaec9985e929d334219b5ff4e07ca210
Dirisu Jesse <dirisujesse@gmail.com>
2017-03-30 14:30:04 +0100

    Update feeds.py

--- polls/feeds.py
+++ polls/feeds.py
@@ -3,9 +3,9 @@ from django.contrib.syndication.views import Feed
 from .models import Question
 
 class PollsFeed(Feed):
-    title_template = 'Polls Feed'
+    title = 'Polls Feed'
     link = '/polls/'
-    description_template = 'Latest Polls Feeds'
+    description = 'Latest Polls Feeds'
     
     def items(self):
         return Question.objects.all()
